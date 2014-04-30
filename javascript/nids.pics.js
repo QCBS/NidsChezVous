aaca7c9221a03b09fdc169299f6ab8e54aa23e01
MohamedYouness <m.youness91@hotmail.com>
2014-04-30 16:20:25 -0400

    Bunch of stuff
    - Pages(next, previous), for all block views complete
    - Ajax for pictures of birds complete
    - Some bugs here and there

--- javascript/nids.pics.js
+++ javascript/nids.pics.js
@@ -1,8 +1,6 @@
 /*global jQuery, window, document, self, _gaq, Drupal, google */
 (function($) {
 
-drupal_set_message('here', 'status');
-
   Drupal.behaviors.nids_map = {
 
     map: {}, map_center: {}, marker_icon: {}, polygon_icon: {},
@@ -19,15 +17,25 @@ drupal_set_message('here', 'status');
         self=$(this);
         bid=$(this).attr('name').replace('espece_', '');
         bid=bid.replace('[]','');
-        $.ajax({
-          url: "nids/oiseaux_pics",
-          data: { oiseau : self.val()[0] },
-          type: "POST",
-          success: function(result){
-              link=result['link'].replace('orig','580_360')
-              $('#photodiv'+bid).html('<a href="'+result['link']+'" target="_blank"><img src="'+link+'" style="height:200px;"></a>')
-          }
-        });
+		if($.isArray(self.val())) {
+		  isval=self.val()[0];
+ 		}else{
+		  isval=self.val();
+		}
+		if(isval !== "Autre" && isval !== "Inconnue" && isval !== ""){
+			$.ajax({
+				url: "nids/oiseaux_pics", 
+				data: { oiseau : isval }, 
+				type: "POST", 
+				success: function(result){
+					link=result['link'].replace('orig','580_360')
+					$('#photodiv'+bid).html('<a href="'+result['link']+'" target="_blank"><img src="'+link+'" style="height:200px;"></a>')
+					}
+			})
+		}else{
+			$('#photodiv'+bid).html('<div id="photodiv0"style="display: none;">')
+			
+		};
       });
     }
   };
